7cf484fffe85b843ddeba1c3ea9f54f3facd8a3e
Rafael Lopez <Vianneyrlm@gmail.com>
2024-11-20 21:05:04 +0100

    amplicacion de la funcion de geolocalizacion para buscar por ciudad

--- js/geo.js
+++ js/geo.js
@@ -1,14 +1,51 @@
 "use strict"
 
+
+
+
+//Nueva funcion interna para mostrar botones si cuando se hace una peticion la localizaciones 
+
+function  elegiropcion(botones) {
+  return new Promise((resolve) => {
+    const menuContainer = document.getElementById("choice-container");
+    menuContainer.innerHTML = ""; // Limpiamos el contenedor
+
+    botones.forEach((boton,indice) => {
+      const buttonElement = document.createElement("button");
+      buttonElement.textContent = boton.display_name;
+      buttonElement.dataset.id = indice;
+
+      // Evento para manejar el clic
+      buttonElement.addEventListener("click", (event) => {
+        const botonPulsado = event.target.dataset.id; // Recuperar el ID del botón pulsado
+        resolve(botonPulsado); // Resuelve la promesa con el ID del botón pulsado
+      });
+
+      menuContainer.appendChild(buttonElement);
+    });
+  });
+}
+
 // Función para obtener la geolocalización con manejo de errores usando try...catch
 // Se cambia la funcion ya que no es opctima y da fallos de carga.
-export  function obtenerUbicacion() {
-  return new Promise((resuelta,rechazada)=>{
-    if (!navigator.geolocation) {
-      rechazada(new Error("La geolocalización no es compatible con este navegador."));
-    }
-    else
+// añadimos mas opciones  
+
+export  async function obtenerUbicacion(modo=1,ciudad="madrid") {
+
+
+  if (modo===1)
+    
     {
+  return new Promise((resuelta,rechazada)=>{
+
+   
+
+
+        if (!navigator.geolocation) {
+            rechazada(new Error("La geolocalización no es compatible con este navegador."));
+         }
+         else
+         {
     // Valores iniciales para a el parametro de geolocation
     const options = {
       enableHighAccuracy: true,
@@ -20,7 +57,7 @@ export  function obtenerUbicacion() {
      function success(pos) {
     
       const crd =  {latitud:pos.coords.latitude ,longitud:pos.coords.longitude};
-   
+      console.log("punto 1",pos.coords)
       resuelta(crd);
     }
 
@@ -32,8 +69,47 @@ export  function obtenerUbicacion() {
     }
       //getCurrentPosition recibe 3 parametros 1 funcion que hacer cuando esta correcta la funcion
     navigator.geolocation.getCurrentPosition(success,error,options);
+  } 
+  
+  }
+ 
+);
+}
+else
+{
+  try
+  { 
+    console.log("punto2")
+    let urlcode ="https://nominatim.openstreetmap.org/search?q={"+ciudad+"}&format=json&accept-language=es";
+    let geodata = await fetch (urlcode);
+    let data = await geodata.json();
+    let posicion={};
+    console.log(data);
+    if (data.length===0)
+    {
+      throw new Error(`La ciudad indicada como ${ciudad} no existe, ¿Estaba bien escrita?`)
+    }
+    else if (data.length===1)
+    {
+       posicion = {latitud:parseFloat(data[0].lat),longitud:parseFloat(data[0].lon)};
+
+    }  
+    else
+    {
+      const opcionelegida = await elegiropcion(data); 
+      posicion = {latitud:parseFloat(data[opcionelegida].lat),longitud:parseFloat(data[opcionelegida].lon)};
+    }
+
+    console.log(posicion);
+
+   
+    return posicion
+  }
+  catch(err1)
+  {
+    throw new Error (err1.message);
   }
 
+
 }
-);
 }
