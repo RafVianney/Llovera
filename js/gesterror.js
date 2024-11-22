06841d72ce47e275c34e7f4d777257c7af35fff9
VikingRacer89 <alanso89@gmail.com>
2024-11-22 11:28:32 +0100

    html endpoint para errores y funcion de gestion de errores

---
+++ js/gesterror.js
@@ -0,0 +1,10 @@
+// Mostrar mensaje de error desde el parámetro URL
+const errorMessageElement = document.getElementById('errorMessage');
+const params = new URLSearchParams(window.location.search);
+const errorMessage = params.get('error');
+errorMessageElement.textContent = errorMessage || 'Error desconocido';
+
+// Botón para volver al inicio
+document.getElementById('backButton').addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
